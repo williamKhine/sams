1f6a27e59baafdb47f4f145eea844111ae10993f
williamKhine <william@khine.dev>
2026-09-22 21:02:42 -0400

    feat: center sign-in and sign-out components

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <SignUp />
+    </div>
+  )
 }
